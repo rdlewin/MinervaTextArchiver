427ac467090826c6775a48bc871d34f6fa7bb9e6
wing2005 <itamargl@gmail.com>
2020-09-11 00:30:01 +0300

    added application -> groups - scrollable with badge added hashtag inline search - retrieve from api for filtering added time range filtering added animations for MessageBox and DiscussionSummary stringToColor avatar (by sender name)

--- ui/src/components/ApplicationsList.js
+++ ui/src/components/ApplicationsList.js
@@ -39,9 +39,30 @@ const styles = (theme) => ({
     },
     groups:{
         display: 'block',
-        width: '100%'
+        width: '100%',
+        maxHeight: '100px',
+        overflow: 'scroll',
+        '&::-webkit-scrollbar': {
+            width: '0.6em'
+        },
+        '&::-webkit-scrollbar-track': {
+            boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+            webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)'
+        },
+        '&::-webkit-scrollbar-thumb': {
+            backgroundColor: 'rgba(255, 255, 255, 0.7)',
+            outline: '1px solid slategrey'
+        }
     }
 });
+
+
+// list: {
+//     overflowY: "auto",
+//         margin: 0,
+//         padding: 0,
+//         listStyle: "none",
+//         height: "100%",
 
 const ITEM_HEIGHT = 48;
 
--- ui/src/components/MessageBox.js
+++ ui/src/components/MessageBox.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import CardHeader from "@material-ui/core/CardHeader";
 import Avatar from "@material-ui/core/Avatar";
-import {getInitials} from "../utils/utils";
+import {getInitials, stringToColour} from "../utils/utils";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import {red} from "@material-ui/core/colors";
 import withStyles from "@material-ui/core/styles/withStyles";
+import Grow from "@material-ui/core/Grow";
 
 
 
@@ -30,25 +31,26 @@ class MessageBox extends Component{
     render() {
         const {classes,data} = this.props;
         return(
+            <Grow in={true}  timeout={1500}>
+                <Card className={classes.root}>
+                    <CardHeader
+                        avatar={
+                            <Avatar aria-label="Discussion" style={{backgroundColor: stringToColour(data.sender_name)}}>
+                                {getInitials(data.sender_name)}
+                            </Avatar>
+                        }
+                        title={`${data.sender_name}`}
+                        subheader={`${data.sent_date}`}
 
-            <Card className={classes.root}>
-                <CardHeader
-                    avatar={
-                        <Avatar aria-label="Discussion" className={classes.avatar}>
-                            {getInitials(data.sender_name)}
-                        </Avatar>
-                    }
-                    title={`${data.sender_name}`}
-                    subheader={`${data.sent_date}`}
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {data.content}
+                        </Typography>
 
-                />
-                <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {data.content}
-                    </Typography>
-
-                </CardContent>
-            </Card>
+                    </CardContent>
+                </Card>
+            </Grow>
         )
     }
 }
